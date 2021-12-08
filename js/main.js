33ad824e93a7522e17e81ca0b2779dd72bd11dd4
YuriiSkill <Rhodenskilled@gmail.com>
2021-12-08 23:55:26 +0200

    creating questions on the user

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let numberOfFilms = prompt('сколько фильмов вы уже посмотрели?');
+// console.log(numberOfFilms);
+
+
+const personalMovieDB = {
+  count:numberOfFilms,
+  movies:{},
+  actors:{},
+  genres:[],
+  privat:false
+};
+
+let a = prompt('один из последних просмотренных фильмов?'),
+    b=prompt('на сколько оцените его?'),
+    c=prompt('один из последних просмотренных фильмов?'),
+    d=prompt('на сколько оцените его?');
+    
+    personalMovieDB.movies[a]=b;
+    personalMovieDB.movies[c]=d;
+
+    console.log(personalMovieDB);
+
+
+// мой вариант
+// let question=[];
+
+// question[0] = prompt('один из последних просмотренных фильмов?');
+// question[1] = prompt('на сколько оцените его?');
+
+// const movies=[question[0],question[1]] ;
+// console.log(movies);
